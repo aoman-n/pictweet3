2d3b7ca292f398605e2aca0c149a1729966a2360
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-06 13:20:24 +0900

    fix form_tag to form_for

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
-    redirect_to "/tweets/#{@comment.tweet.id}"
+    @comment = Comment.create(text: comment_params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
+    redirect_to tweet_path(params[:tweet_id])
   end
 
   private
   def comment_params
-    params.permit(:text, :tweet_id)
+    params.require(:comment).permit(:text)
   end
 end
