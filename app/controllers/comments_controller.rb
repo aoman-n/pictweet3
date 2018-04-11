4347cde7a33865cf8cce53c90ad1a8c905dbd960
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-11 17:22:14 +0900

    Tweet.comment ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,23 @@
 class CommentsController < ApplicationController
+
   def create
-    @comment = Comment.create(text: comment_params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
-    redirect_to tweet_path(params[:tweet_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to tweet_path(params[:tweet_id]) }
+        format.json
+      end
+      flash[:notice] = 'コメントが送信されました'
+    else
+       # @tweet = Tweet.find(params[:tweet_id])
+       # @comments = @tweet.comments.includes(:user)
+      # flash.now[:alert] = 'メッセージを入力してください'
+       render 'tweets/show'
+     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text).merge(tweet_id: params[:tweet_id], user_id: current_user.id)
   end
 end
---
+++ app/views/comments/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.text  @comment.text
+json.id  @comment.user.id
+json.user_name  @comment.user.nickname
+
+#右のものを左に代入
+#つまり、@commentのインスタンスのデータをjson形式にしている
